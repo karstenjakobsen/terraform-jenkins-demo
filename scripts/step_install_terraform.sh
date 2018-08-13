8090680c4b3ae34e833494a4b08a7983b7105e6e
Karsten Jakobsen <karsten@karstenjakobsen.dk>
2018-08-13 14:52:57 +0200

    Update step_install_terraform.sh

--- scripts/step_install_terraform.sh
+++ scripts/step_install_terraform.sh
@@ -3,6 +3,7 @@
 echo "Installing Terraform v0.11.07"
 unzip packages/terraform/terraform_0.11.7_linux_amd64.zip -d bin/
 
-./bin/terraform --version
-
 ln -s bin/terraform /usr/local/bin/terraform
+
+which terraform
+terraform --version
