2ea3d56a96e272ac646c5e0ba0c1f86eef20139c
Karsten Jakobsen <karsten@karstenjakobsen.dk>
2018-08-13 15:07:25 +0200

    Update step_install_terraform.sh

--- scripts/step_install_terraform.sh
+++ scripts/step_install_terraform.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Installing Terraform v0.11.07"
-unzip packages/terraform/terraform_0.11.7_linux_amd64.zip -d bin/
+INSTALL_DIR=$WORKDIR/bin
 
-./bin/terraform --version
+echo "Installing Terraform v0.11.07"
+unzip packages/terraform/terraform_0.11.7_linux_amd64.zip -d $INSTALL_DIR
+
+$INSTALL_DIR/terraform --version
