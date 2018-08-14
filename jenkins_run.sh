94d7f81d5b463c347c7d05ae742994c1a0b8f9b5
Karsten Kaj Jakobsen <kj@patientsky.com>
2018-08-14 13:27:52 +0200

    Rermoved pakcages - we now use docker image

--- jenkins_run.sh
+++ jenkins_run.sh
@@ -3,14 +3,15 @@
 docker stop jenkins-docker
 docker rm jenkins-docker
 
-mkdir /root/jenkins_data
-chmod 777 /root/jenkins_data
+mkdir /var/jenkins_home
+chmod 777 /var/jenkins_home
 
 docker run --net=host --name=jenkins-docker -d --cap-add=ALL --privileged \
 -p 8080:8080 \
 -p 50000:50000 \
--v /root/jenkins_data:/var/jenkins_home \
+-v /var/jenkins_home:/var/jenkins_home \
 -v /var/run/docker.sock:/var/run/docker.sock \
+-v /root/.ssh:/root/.ssh \
 -e VSPHERE_PASSWORD=test1234 \
 -e TERRAFORM_IMAGE="pasientskyhosting/terraform-docker:latest" \
 pasientskyhosting/jenkins-docker:0.0.1-build-1
