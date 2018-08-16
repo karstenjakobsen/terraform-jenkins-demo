9a898b803051bf3ee72840de3e306e07432f438d
Karsten Jakobsen <karsten@karstenjakobsen.dk>
2018-08-16 14:13:42 +0200

    Update vm.tf

--- terraform/odn1/hp/voip/vm.tf
+++ terraform/odn1/hp/voip/vm.tf
@@ -1,6 +1,6 @@
 module "voip_test" {
   
-  source = "git@github.com:pasientskyhosting/terraform-modules.git//modules/vsphere_vm?ref=v5.0.0-rc3"
+  source = "git@github.com:pasientskyhosting/terraform-modules.git//modules/vsphere_vm?ref=v5.0.0"
 
   datacenter = "${data.vsphere_datacenter.datacenter.name}"
 
